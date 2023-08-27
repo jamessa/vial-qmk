27740930e1de19ec07544ba5761c41733a215e45
James Sa <james.sa.2013@gmail.com>
2023-08-27 19:33:58 +0800

    common scan matrix, enable caps word, move to json settings

--- keyboards/jsa/pragmatic56a/keymaps/vial/rules.mk
+++ keyboards/jsa/pragmatic56a/keymaps/vial/rules.mk
@@ -1,9 +1,5 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
-VIAL_INSECURE = yes
 QMK_SETTINGS = yes
 
 EXTRAFLAGS += -flto
-
-CUSTOM_MATRIX = lite
-SRC += matrix.c
--- keyboards/jsa/pragmatic56a/pragmatic56a.c
+++ keyboards/jsa/pragmatic56a/pragmatic56a.c
@@ -14,4 +14,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "pragmatic56.h"
+#include "pragmatic56a.h"
