58ff2085cfe7303fcc5e24dc1c4fa576a53326df
james <188772+jamessa@users.noreply.github.com>
2024-05-17 19:54:06 +0800

    working

--- keyboards/jsa/justtype/keymaps/vial/keymap.c
+++ keyboards/jsa/justtype/keymaps/vial/keymap.c
@@ -2,10 +2,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    QK_BOOT,
-        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-        KC_COMMA,KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_Z,    KC_DOT,  KC_TAB,
-        KC_A,    KC_B,    KC_BACKSPACE
+        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    
+        KC_J,    KC_L,    KC_U,    KC_Y,    KC_1,
+        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    
+        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
+        KC_3,    KC_X,    KC_C,    KC_D,
     )
 };               
 
