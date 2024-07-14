ffe0b3afc6bed34916b9776cbb9e72d3d89433f0
james <188772+jamessa@users.noreply.github.com>
2024-07-14 21:36:38 +0800

    fix R4

--- keyboards/jsa/justtype/keymaps/vial/keymap.c
+++ keyboards/jsa/justtype/keymaps/vial/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_1, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_3, KC_X, KC_C, KC_D, )};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(
+                                                                  KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_1, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_3, KC_X, KC_C, KC_D, )};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
